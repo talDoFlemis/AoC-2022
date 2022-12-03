a48ada354fd45086bd960b514c53da6ef3e19a1c
taldoflemis <coderflemis@gmail.com>
2022-12-03 09:25:25 -0300

    removing rucksack struct for day3

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,88 +1,58 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, time::Instant};
 
-struct Rucksack {
-    data: String,
-    shared_item: u32,
+fn get_intersection(a: &str, b: &str) -> String {
+    let set1: HashSet<char> = HashSet::from_iter(a.chars());
+    let set2: HashSet<char> = HashSet::from_iter(b.chars());
+    let res = set1.intersection(&set2);
+    String::from_iter(res)
 }
 
-impl Rucksack {
-    fn new(input: &str) -> Self {
-        let size = input.len();
-        let left = &input[0..size / 2];
-        let right = &input[size / 2..size];
-        let set1: HashSet<char> = right.chars().collect();
-        let common = left.chars().find(|c| set1.contains(c)).unwrap();
+fn get_common_item(input: &str) -> u32 {
+    let size = input.len();
+    let left = &input[..size / 2];
+    let right = &input[size / 2..];
 
-        Rucksack {
-            data: String::from(input),
-            shared_item: Self::get_priority(common),
-        }
-    }
+    let char = get_intersection(left, right).chars().next().unwrap();
 
-    fn get_priority(c: char) -> u32 {
-        if c.is_lowercase() {
-            (c as u8 - 96).into()
-        } else {
-            (c as u8 - 38).into()
-        }
+    get_priority(char)
+}
+
+fn get_priority(c: char) -> u32 {
+    if c.is_lowercase() {
+        (c as u8 - 96).into()
+    } else {
+        (c as u8 - 38).into()
     }
 }
 
-struct Group {
-    common_badge: u32,
-}
+fn get_badge(s1: &str, s2: &str, s3: &str) -> u32 {
+    let common_s1_s2 = get_intersection(s1, s2);
+    let common = get_intersection(&common_s1_s2, s3).chars().next().unwrap();
 
-impl Group {
-    fn new(input: &str) -> Self {
-        let vec: Vec<Rucksack> = input.lines().map(Rucksack::new).take(3).collect();
-        let set1: HashSet<char> = vec[1].data.chars().collect();
-        let set2: HashSet<char> = vec[2].data.chars().collect();
-        let common = vec[0]
-            .data
-            .chars()
-            .filter(|c| set1.contains(c))
-            .collect::<String>();
-        let common = common.chars().find(|c| set2.contains(c)).unwrap();
-        println!("Common is {common:?}");
-
-        Group {
-            common_badge: Rucksack::get_priority(common),
-        }
-    }
+    get_priority(common)
 }
 
 fn part1(input: &str) -> u32 {
-    input.lines().map(|l| Rucksack::new(l).shared_item).sum()
+    input.lines().map(get_common_item).sum()
 }
 
 fn part2(input: &str) -> u32 {
-    let mut counter = 0;
+    let lines = input.lines().collect::<Vec<&str>>();
     let mut sum = 0;
-    let mut string = String::new();
-    for line in input.lines() {
-        if counter == 2 {
-            string.push_str(line);
-            string.push('\n');
-            println!("Group is {string}");
-
-            sum += Group::new(&string).common_badge;
-            string = "".to_string();
-            counter = 0;
-        } else {
-            string.push_str(line);
-            string.push('\n');
-            println!("String is {string}");
-            counter += 1;
-        }
+    for (idx, _) in lines.iter().enumerate().step_by(3) {
+        sum += get_badge(lines[idx], lines[idx + 1], lines[idx + 2]);
     }
 
     sum
 }
 
 fn main() {
+    let start = Instant::now();
     let data = include_str!("../day3.txt");
     let sum1 = part1(data);
     let sum2 = part2(data);
+    let end = Instant::now();
     println!("The sum of priorities is {sum1}");
     println!("The sum of badges is {sum2}");
+    println!("Duration is {:?}", end.duration_since(start));
 }
